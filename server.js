a6dae8131140e017d6fcd5e14ea2a60e52af7600
muna alshorman <munaalshorman123@gmail.com>
2020-02-04 03:26:38 +0200

    can signup & signin

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/signin', basicAuth, (req, res) => {
 
 app.get('/users', basicAuth, (req, res) => {
   console.log('kkkkkkkk',(users.find()));
-  res.status(200).json(users.find());
+  res.status(200).json();
 });
 
 module.exports = {
